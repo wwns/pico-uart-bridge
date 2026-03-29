7dd078acc5cb3813f6f76b4ba3451d87840660c3
wwns <wieslawweglowski@gmail.com>
2026-03-29 11:50:17 +0200

    Remove RTS/CTS - no flow control

--- main_simple.py
+++ main_simple.py
@@ -3,8 +3,6 @@ Raspberry Pi Pico - Prosty UART bridge bez menu
 ================================================
 Pico GP0 (TX, pin 1)  -> (RX) urzadzenia
 Pico GP1 (RX, pin 2)  <- (TX) urzadzenia
-Pico GP2 (CTS, pin 4) <- (RTS) urzadzenia  (opcjonalne)
-Pico GP3 (RTS, pin 5) -> (CTS) urzadzenia  (opcjonalne)
 Pico GND  (pin 3)     -> GND
 Zmien BAUDRATE na potrzebny.
 """
@@ -14,7 +12,7 @@ from machine import UART, Pin
 BAUDRATE = 115200
 
 micropython.kbd_intr(-1)
-uart = UART(0, baudrate=BAUDRATE, tx=Pin(0), rx=Pin(1), rts=Pin(3), cts=Pin(2), timeout=0)
+uart = UART(0, baudrate=BAUDRATE, tx=Pin(0), rx=Pin(1), timeout=0)
 led  = Pin(25, Pin.OUT)
 poll = uselect.poll()
 poll.register(sys.stdin, uselect.POLLIN)
